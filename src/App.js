8ca740cc86fc9ddab58ad24159c2d5fb24622487
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-31 07:58:34 -0700

    do some practice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import React, { Component } from "react";
-import JSXVariables from "./components/JSXVariables";
-import FilmsList from "./components/FilmsList"
+import React from 'react';
+import Thermostat from './components/Thermostat';
 
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Studio Ghibli Films</h1>
-        <FilmsList />
-      </div>
-    );
-  }
+function App() {
+  return <Thermostat />;
 }
+
 export default App;
